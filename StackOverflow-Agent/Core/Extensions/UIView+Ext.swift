87ce02c71d858396ea8355994d1d415828c52ebe
Shady K. Maadawy <61798847+shadudiix@users.noreply.github.com>
2023-02-24 11:58:13 +0200

    create uiview extension and write a func to disable auto reizing mask

---
+++ StackOverflow-Agent/Core/Extensions/UIView+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Ext.swift
+//  StackOverflow-Agent
+//
+//  Created by Shady K. Maadawy on 24/02/2023.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func disableAutoResizingMask() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
